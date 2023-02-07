eaeb71b0e3b1015e1cd90b8c088b8a37effd68a5
liza <123323177+younggengaar@users.noreply.github.com>
2023-02-07 10:45:52 +0200

    Add files via upload

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание1
+            //int[] omas = new int[8];
+            //for (int i=0; i<8; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            // omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for(int i=0;i<8;i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadKey();
+
+            //задание2
+            //int[] mas = new int[8] { 1, -1, 2, -2, 3, -3, 4, -4 };
+            //for(int i = 0; i < 8; i++)
+            //{
+            //    if(mas[i] > 0)
+            //    {
+            //        Console.WriteLine(mas[i]);
+            //    }
+            //}
+            //Console.ReadKey();
+
+            //задание3
+            //int[] omas = new int[6] { -7, 10, 5, 16, -4, 18 };
+            //for(int i=omas.Length-1;i>=0;--i)
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //Console.ReadKey();
+
+            //задание4
+            //Random au = new Random();
+            //int a = au.Next(163, 190);
+            //int[] omas = new int[12];
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    omas[i] = au.Next(163, 190);
+            //}
+            //for(int i=0;i<12;i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
